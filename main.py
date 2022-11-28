0b25ae98f632ba2d733783cd6b85911c5cfd2bf5
rickkun11 <rickkun11@gmail.com>
2022-11-29 01:28:22 +0700

    add

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-from symspellpy.symspellpy import SymSpell, Verbosity
+from symspellpy import SymSpell, Verbosity
 
-  # initialize
-  sym_spell = SymSpell()
-  
-  # create dictionary
-  path_corpus = "kompas.txt"
-  sym_spell.create_dictionary(path_corpus)
-  
-  #lookup suggestions for multi-word input strings (supports compound splitting & merging)
-  list_kata = Element('textinput')
-  sentence = list_kata.split()
-  
-  for kata in sentence:
-      maxEditDistanceLookup = 2
-      include_unknown = True
-      suggestion = sym_spell.lookup(kata, Verbosity.TOP, maxEditDistanceLookup, include_unknown)
-  
-  #display term and edit distance
-      for s in suggestion:
-          getTextOutput = Element("textoutput")
-          getTextOutput.write(s.term, end=' ')
+# initialize
+sym_spell = SymSpell()
+
+# create dictionary
+path_corpus = "kompas.txt"
+sym_spell.create_dictionary(path_corpus)
+
+#lookup suggestions for multi-word input strings (supports compound splitting & merging)
+list_kata = 'Bhasa merupakan alat komonikasi yang sunguh penting. Melalui bhasa manusia dpat menyampaikan isi hatii kepda sesamanya, mewarisi dan mewariskan, menerimo dan memberi pengtahuan kepada sesamanya. Bahkn dengn bahsa pula mnusia dapat mengekspresokan jiwah seninya. Dengn demikian jelaslah bhwa bahasab nerupakan sarana komunikasi yang sngat penting dlam kehidupan manusio.'
+sentence = list_kata.split()
+
+for kata in sentence:
+    maxEditDistanceLookup = 2
+    include_unknown = True
+    suggestion = sym_spell.lookup(kata, Verbosity.TOP, maxEditDistanceLookup, include_unknown)
+
+#display term and edit distance
+    for s in suggestion:
+        print(s.term, end=' ')
