90a336adb38db28cb4b592fc3048e10aa3475e18
rickkun11 <rickkun11@gmail.com>
2022-11-29 05:26:14 +0700

    add

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 @jsf.use(app)
 class App:
     def __init__(self):
-        self.js.console.log(list_kata)
+        
 
     
 @app.route('/')
 def index():
-        return App.render(render_template('index.html'))
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
