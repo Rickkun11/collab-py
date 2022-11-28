7b18ee08a2383931ec85a52148e45777ec886f7d
rickkun11 <rickkun11@gmail.com>
2022-11-29 04:05:35 +0700

    add

--- main.py
+++ main.py
@@ -1,21 +1,48 @@
+from flask import Flask
+from flask import render_template
+import jyserver.Flask as jsf
 from symspellpy import SymSpell, Verbosity
 
-# initialize
-sym_spell = SymSpell()
 
-# create dictionary
-path_corpus = "kompas.txt"
-sym_spell.create_dictionary(path_corpus)
+app = Flask(__name__)
+class App:
+    def __init__(self):
+        # initialize
+        sym_spell = SymSpell()
 
-#lookup suggestions for multi-word input strings (supports compound splitting & merging)
-list_kata = 'Bhasa merupakan alat komonikasi yang sunguh penting. Melalui bhasa manusia dpat menyampaikan isi hatii kepda sesamanya, mewarisi dan mewariskan, menerimo dan memberi pengtahuan kepada sesamanya. Bahkn dengn bahsa pula mnusia dapat mengekspresokan jiwah seninya. Dengn demikian jelaslah bhwa bahasab nerupakan sarana komunikasi yang sngat penting dlam kehidupan manusio.'
-sentence = list_kata.split()
+        # create dictionary
+        path_corpus = "kompas.txt"
+        sym_spell.create_dictionary(path_corpus)
 
-for kata in sentence:
-    maxEditDistanceLookup = 2
-    include_unknown = True
-    suggestion = sym_spell.lookup(kata, Verbosity.TOP, maxEditDistanceLookup, include_unknown)
+        #lookup suggestions for multi-word input strings (supports compound splitting & merging)
+        list_kata = ''
+        sentence = list_kata.split()
 
-#display term and edit distance
-    for s in suggestion:
-        print(s.term, end=' ')
+        for kata in sentence:
+            maxEditDistanceLookup = 2
+            include_unknown = True
+            suggestion = sym_spell.lookup(kata, Verbosity.TOP, maxEditDistanceLookup, include_unknown)
+
+        #display term and edit distance
+            for s in suggestion:
+                print(s.term, end=' ')
+    
+@app.route('/')
+def index():
+        return App.render(render_template('index.html'))
+
+if __name__ == '__main__':
+     app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
