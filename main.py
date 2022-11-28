89b19aca9082046d9dce90bf4dec7bb246359e09
rickkun11 <rickkun11@gmail.com>
2022-11-29 04:48:19 +0700

    add

--- main.py
+++ main.py
@@ -1,36 +1,12 @@
 from flask import Flask
 from flask import render_template
 import jyserver.Flask as jsf
-from symspellpy import SymSpell, Verbosity
-
+from spelling import *
 
 app = Flask(__name__)
 class App:
     def __init__(self):
-        # initialize
-        sym_spell = SymSpell()
-
-        # create dictionary
-        path_corpus = "kompas.txt"
-        sym_spell.create_dictionary(path_corpus)
-
-        #lookup suggestions for multi-word input strings (supports compound splitting & merging)
-        list_kata = 'awiwkok'
-        sentence = list_kata.split()
-
-        for kata in sentence:
-            maxEditDistanceLookup = 2
-            include_unknown = True
-            suggestion = sym_spell.lookup(kata, Verbosity.TOP, maxEditDistanceLookup, include_unknown)
-
-        #display term and edit distance
-            for s in suggestion:
-                print(s.term, end=' ')
-        
-        textoutput = self.js.document.getElementById("outputtext")
-        textinput = self.js.document.getElementById("textinput").innerHTML = list_kata
-        
-        self.console.log(textinput)
+        self.console.log(list_kata)
 
     
 @app.route('/')
@@ -38,7 +14,7 @@ def index():
         return App.render(render_template('index.html'))
 
 if __name__ == '__main__':
-     app.run()
+app.run()
 
 
 
---
+++ spelling.py
@@ -0,0 +1,20 @@
+from symspellpy import SymSpell, Verbosity
+# initialize
+sym_spell = SymSpell()
+# create dictionary
+path_corpus = "kompas.txt"
+sym_spell.create_dictionary(path_corpus)
+
+#lookup suggestions for multi-word input strings (supports compound splitting & merging)
+list_kata = 'awiwkok'
+sentence = list_kata.split()
+
+for kata in sentence:
+            maxEditDistanceLookup = 2
+            include_unknown = True
+            suggestion = sym_spell.lookup(kata, Verbosity.TOP, maxEditDistanceLookup, include_unknown)
+
+        #display term and edit distance
+            for s in suggestion:
+                print(s.term, end=' ')
+        
