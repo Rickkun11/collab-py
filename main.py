ba76de4a8c9e504239edb4cc81053478112f28c3
rickkun11 <rickkun11@gmail.com>
2022-11-30 05:30:25 +0700

    change all , update to flask

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-from flask import Flask
-from flask import render_template
-import jyserver.Flask as jsf
-from spelling import *
+from flask import Flask, request, render_template, url_for, redirect
+from symspellpy import SymSpell, Verbosity
+import pkg_resources
 
 app = Flask(__name__)
 
-@jsf.use(app)
-class App:
-    def __init__(self): 
-        self.count = 0
 
 @app.route('/')
 def index():
-        return App.render(render_template('index.html'))
+    return render_template('index.html')
 
+@app.route('/deteksi')
+def deteksi():
+    return render_template('deteksi.html')
+
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    hasil = request.form['textoutput']
+    input = request.form['textinput']
+
+    
+    #lookup suggestions for multi-word input strings (supports compound splitting & merging)
+    sentence = input.split()
+    #initialize
+    sym_spell = SymSpell()
+    # create dictionary
+    path_corpus = "kompas.txt"
+    sym_spell.create_dictionary(path_corpus)
+
+    for kata in sentence:
+                suggestion = suggestions = sym_spell.lookup_compound(
+    input, max_edit_distance=2, transfer_casing=True,
+)
+
+            #display term and edit distance
+                for suggestion in suggestion:
+                    hasil = suggestion
+    return render_template('deteksi.html', hasil = hasil, input=input )
 if __name__ == '__main__':
     app.run()
--- templates/index.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__, template_folder='templateFiles')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
- 
-if __name__=='__main__':
-    app.run(debug = True)
